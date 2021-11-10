3f5f018a785f887f518a7c79946cdb0111c40b47
kapangyu-gsa <kapang.yu@gsa.gov>
2021-11-10 15:14:53 -0500

    Added new file fpds_feed.py

---
+++ fpds_feed.py
@@ -0,0 +1,29 @@
+from urllib.parse import quote_plus
+from lxml import etree
+import pandas as pd
+from io import StringIO
+import requests
+
+class fpds_feed_reader():
+    def __init__(self, xslt_file: str="fpds_feed.xsl"):
+        self.xslt_file = xslt_file
+        self.base_url = "https://www.fpds.gov/ezsearch/fpdsportal?s=FPDS.GOV&indexName=awardfull&templateName=1.5.2&rss=1&feed=atom0.3&q=+LAST_MOD_DATE"
+        
+    
+    def get_data(self, last_mod_date_start: str="2021/10/01", last_mod_date_end: str="2021/11/01"):
+        # TODO: validate the input arguments
+        param = f':[{last_mod_date_start},{last_mod_date_end}]'
+        url = self.base_url + quote_plus(param)
+        response = requests.get(url)
+        if (response.ok == False):
+            response.raise_for_status()
+            
+        doc = etree.XML(response.content)
+        xslt_tree = etree.parse(self.xslt_file)
+        transform = etree.XSLT(xslt_tree)
+        result = transform(doc)
+        csv_str = str(result)
+        df = pd.read_csv(StringIO(csv_str), sep="\t", header=None, index_col=0)
+        return df
+
+        
