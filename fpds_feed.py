ae3dc3d64e07e49ae74a5bb8a0b16c68ee219b87
kapangyu-gsa <kapang.yu@gsa.gov>
2021-12-15 09:26:03 -0500

    Added number of threads argument

--- fpds_feed.py
+++ fpds_feed.py
@@ -5,13 +5,17 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("s", help="search keyword")
     parser.add_argument("v", help="keyword value or start value")
-    parser.add_argument("-u", help="end value")
+    parser.add_argument("-o", help="end value")
+    parser.add_argument("-w", help="number of threads")
     args = parser.parse_args()
               
-    param = f'{args.s}:"{args.v}"' if (args.u is None) else f'{args.s}:[{args.v},{args.u}]'
+    param = f'{args.s}:"{args.v}"' if (args.o is None) else f'{args.s}:[{args.v},{args.o}]'
     reader = feed_reader()
-    reader.get_data(param)
+    if (args.w is None):
+        reader.get_data(param)    
+    else:
+        reader.get_data(param, int(args.w))
     
-         
+
 if __name__ == "__main__":
     main(sys.argv[1:])        
